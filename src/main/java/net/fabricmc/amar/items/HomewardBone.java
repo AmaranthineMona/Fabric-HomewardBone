baa405919a5d7e5bb438b7319e7aaff57b1f5bf5
AmaranthineMona <mona.a.kap@gmail.com>
2022-07-22 03:48:35 -0400

    Homeward bone cleanup and tweaks

--- src/main/java/net/fabricmc/amar/items/HomewardBone.java
+++ src/main/java/net/fabricmc/amar/items/HomewardBone.java
@@ -6,7 +6,9 @@ import net.fabricmc.amar.EntityExt;
 import net.fabricmc.amar.HomeAnchor;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -23,7 +25,8 @@ public class HomewardBone extends Item {
     public HomewardBone() {
         super(new FabricItemSettings()
                 .group(ItemGroup.BREWING)
-                .maxCount(16));
+                .maxCount(16)
+                .food(new FoodComponent.Builder().hunger(0).saturationModifier(0).snack().alwaysEdible().build()));
     }
 
     public HomewardBone(Settings settings) {
@@ -34,25 +37,29 @@ public class HomewardBone extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         player.playSound(SoundEvents.BLOCK_FIRE_AMBIENT, 1.0f, 1.0f);
 
+        return super.use(world, player, hand);
+    }
+
+    private void TeleportPlayer(World world, LivingEntity player, Hand hand) {
         if (!world.isClient) {
             var anchorPos = ((EntityExt) player).GetHomeAnchorPos();
             var block = world.getBlockState(anchorPos).getBlock();
 
-            if (block instanceof HomeAnchor) {
-                if (world.getRegistryKey().getValue() == DimensionType.OVERWORLD_ID) {
+            if (world.getRegistryKey().getValue().equals(DimensionType.OVERWORLD_ID)) {
+                if (block instanceof HomeAnchor) {
+
                     ((ServerPlayerEntity) player).networkHandler.requestTeleport(anchorPos.getX(), anchorPos.getY(),
                             anchorPos.getZ(), player.getYaw(), player.getPitch());
-                            
+
                     player.getStackInHand(hand).decrement(1);
                 } else {
-                    player.sendSystemMessage(Text.of("Home anchor is in another dimension."), player.getUuid());
+                    player.sendSystemMessage(Text.of("Home anchor was broken or blocked."), player.getUuid());
                 }
             } else {
-                player.sendSystemMessage(Text.of("Home anchor was broken or blocked."), player.getUuid());
+                player.sendSystemMessage(Text.of("Home anchor is in another dimension."), player.getUuid());
             }
 
         }
-        return super.use(world, player, hand);
     }
 
     @Override
@@ -62,6 +69,15 @@ public class HomewardBone extends Item {
 
     @Override
     public UseAction getUseAction(ItemStack stack) {
-        return UseAction.SPEAR;
+        return UseAction.BOW;
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity player, ItemStack stack, int remainingUseTicks) {
+        if (remainingUseTicks == 1) {
+            TeleportPlayer(world, player, player.getActiveHand());
+
+            player.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 2.0f, 0.75f);
+        }
     }
 }
