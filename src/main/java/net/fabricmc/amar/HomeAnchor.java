23b0eb359052a72e4ba74d571c14853396cf7467
AmaranthineMona <mona.a.kap@gmail.com>
2022-07-22 04:10:19 -0400

    Home anchor cleanup and tweaks

--- src/main/java/net/fabricmc/amar/HomeAnchor.java
+++ src/main/java/net/fabricmc/amar/HomeAnchor.java
@@ -4,49 +4,49 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
+import net.minecraft.world.dimension.DimensionType;
 
 public class HomeAnchor extends Block {
     public HomeAnchor(Settings settings) {
         super(settings);
     }
 
-    public HomeAnchor(){
+    public HomeAnchor() {
         super(FabricBlockSettings.of(Material.STONE).strength(1.0f).requiresTool());
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
             BlockHitResult hit) {
-        if (!world.isClient){
+        if (!world.isClient) {
             ((EntityExt) player).UpdateAnchor(pos);
-
-            player.sendMessage(Text.of("awwoooo"), false);
         }
 
         return ActionResult.SUCCESS;
     }
 
     @Override
-    public void onBlockBreakStart(BlockState state, World world, BlockPos pos, PlayerEntity player) {
-        if (!world.isClient){
-            var heck = ((EntityExt) player).GetHomeAnchorPos();
-
-            if (heck != null){
-
-                player.sendMessage(Text.of(heck.toShortString()), false);
-            }else{
-                player.sendMessage(Text.of("oh no"), false);
-
+    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity player, ItemStack itemStack) {
+        if (!world.isClient) {
+            if (world.getRegistryKey().getValue().equals(DimensionType.OVERWORLD_ID)) {
+                super.onPlaced(world, pos, state, player, itemStack);
+            } else {
+                sendMessage(player, "Can only be placed in the overworld");
+                world.breakBlock(pos, player.canTakeDamage(), player, 2);
             }
         }
     }
-}
 
+    private void sendMessage(LivingEntity player, String text) {
+        player.sendSystemMessage(Text.of(text), player.getUuid());
+    }
+}
