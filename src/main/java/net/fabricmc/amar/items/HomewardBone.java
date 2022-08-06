5f3d1f89bdd146fc7a7b60facf1657a7e8007f85
AmaranthineMona <mona.a.kap@gmail.com>
2022-08-06 01:48:15 -0400

    Homeward bones now teleport to a safe location if possible.

--- src/main/java/net/fabricmc/amar/items/HomewardBone.java
+++ src/main/java/net/fabricmc/amar/items/HomewardBone.java
@@ -5,9 +5,11 @@ import java.util.List;
 import net.fabricmc.amar.blocks.Bonfire;
 import net.fabricmc.amar.util.EntityExt;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.AirBlock;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.LavaFluid;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -19,10 +21,15 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction.Axis;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 
 public class HomewardBone extends Item {
+    final double PLAYER_BLOCK_OFFSET = 0.5D;
+
     public HomewardBone() {
         super(new FabricItemSettings()
                 .group(ItemGroup.BREWING)
@@ -72,6 +79,72 @@ public class HomewardBone extends Item {
         return block instanceof Bonfire;
     }
 
+    private int getFallDistance(BlockPos startPos, World world) {
+        var fallDistance = 1;
+        var currentBlock = startPos.add(0, -1, 0);
+        while (world.getBlockState(currentBlock).getBlock() instanceof AirBlock) {
+            fallDistance++;
+            currentBlock = currentBlock.add(0, -1, 0);
+        }
+
+        return fallDistance;
+    }
+
+    private boolean isHalfBlockOrSmaller(VoxelShape shape) {
+        var min = shape.getMin(Axis.Y);
+        var max = shape.getMax(Axis.Y);
+
+        if (Double.isNaN(min) || Double.isNaN(max))
+            return true;
+
+        return (max - min) <= 0.5D;
+    }
+
+    private boolean isFloorBlockTeleportSafe(World world, BlockPos footPos) {
+        var footBlockState = world.getBlockState(footPos);
+        var footBlockFluidState = footBlockState.getFluidState();
+
+        if (footBlockFluidState.getFluid() instanceof LavaFluid) {
+            return false;
+        }
+
+        if (!(footBlockState.getBlock() instanceof AirBlock)
+                && !isHalfBlockOrSmaller(footBlockState.getCollisionShape(world, footPos))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private BlockPos getTeleportLocation(BlockPos anchorPos, World world, PlayerEntity player) {
+        for (int width = -1; width <= 1; width++) {
+            for (int depth = -1; depth <= 1; depth++) {
+                if (width == 0 && depth == 0) {
+                    continue;
+                }
+
+                var foot = anchorPos.add(width, 0, depth);
+                if (!isFloorBlockTeleportSafe(world, foot)) {
+                    continue;
+                }
+
+                var head = anchorPos.add(width, 1, depth);
+                if (!((world.getBlockState(head)).getBlock() instanceof AirBlock)) {
+                    continue;
+                }
+
+                var floor = anchorPos.add(width, -1, depth);
+                if ((world.getBlockState(floor)).getBlock() instanceof AirBlock && getFallDistance(floor, world) >= 3) {
+                    continue;
+                }
+
+                return foot;
+            }
+        }
+
+        return null;
+    }
+
     private void TeleportPlayer(World world, LivingEntity player, Hand hand) {
         if (!world.isClient) {
             if (player.hasVehicle()) {
@@ -80,11 +153,17 @@ public class HomewardBone extends Item {
 
             var anchorPos = ((EntityExt) player).GetHomeAnchorPos();
             var homeAnchor = (Bonfire) world.getBlockState(anchorPos).getBlock();
-            ((ServerPlayerEntity) player).networkHandler.requestTeleport(anchorPos.getX(), anchorPos.getY(),
-                    anchorPos.getZ(), player.getYaw(), player.getPitch());
+            var teleportLocation = getTeleportLocation(anchorPos, world, (PlayerEntity) player);
 
-            homeAnchor.onTeleport(anchorPos, world);
-            player.getStackInHand(hand).decrement(1);
+            if (teleportLocation != null) {
+                ((ServerPlayerEntity) player).networkHandler.requestTeleport(teleportLocation.getX() + PLAYER_BLOCK_OFFSET,
+                        teleportLocation.getY(), teleportLocation.getZ() + PLAYER_BLOCK_OFFSET, player.getYaw(), player.getPitch());
+
+                homeAnchor.onTeleport(anchorPos, world);
+                player.getStackInHand(hand).decrement(1);
+            } else {
+                ((PlayerEntity) player).sendMessage(new TranslatableText("gameplay.homeward.bonfire_blocked"), true);
+            }
         }
     }
 
